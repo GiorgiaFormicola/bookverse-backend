38379932e6d270188f73ee77f434ddcaa6c3e2e5
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-05-11 16:34:18 +0200

    Adds Errors Handler and errors DTO

---
+++ src/main/java/giorgiaformicola/capstone/exceptions/ErrorsHandler.java
@@ -0,0 +1,39 @@
+package giorgiaformicola.capstone.exceptions;
+
+import giorgiaformicola.capstone.payloads.ErrorDTO;
+import giorgiaformicola.capstone.payloads.ErrorsListDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorsHandler {
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleBadRequestException(BadRequestException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(PayloadValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsListDTO handlePayloadValidationException(PayloadValidationException ex) {
+        return new ErrorsListDTO(ex.getMessage(), LocalDateTime.now(), ex.getErrors());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorDTO handleUnauthorizedException(UnauthorizedException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handleNotFoundException(NotFoundException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    //TODO: handle missing errors
+}
---
+++ src/main/java/giorgiaformicola/capstone/payloads/ErrorDTO.java
@@ -0,0 +1,9 @@
+package giorgiaformicola.capstone.payloads;
+
+import java.time.LocalDateTime;
+
+public record ErrorDTO(
+        String message,
+        LocalDateTime timestamp
+) {
+}
