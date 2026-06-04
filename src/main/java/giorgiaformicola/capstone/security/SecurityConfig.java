2ef1c16a147a8dfb613a9071bfe1f8bc65b620f3
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-06-05 01:49:00 +0200

    Updates CORS SecurityConfig

--- src/main/java/giorgiaformicola/capstone/security/SecurityConfig.java
+++ src/main/java/giorgiaformicola/capstone/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(@Value("${localhost.port}") String port, @Value("${server.address}") String address, @Value("${frontend.url}") String frontendURL) {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:" + port, "http://" + address + ":" + port, frontendURL));
+        configuration.setAllowedOrigins(List.of("http://localhost:" + port, "http://" + address + ":" + port, frontendURL, "https://*.vercel.app"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
 
